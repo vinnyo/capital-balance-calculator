071cb53952b05a03713dd462cbd08997505b34b8
Marvin Yoong <marvin@place.technology>
2023-02-08 22:35:31 +1100

    chore: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
 module.exports = {
+  mode: 'jit',
+  important: '#sayf',
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    colors:{
+      gray: colors.gray
+    },
     extend: {},
   },
   plugins: [],
